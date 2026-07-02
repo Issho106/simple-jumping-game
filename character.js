f323a9a09a3411000b11b97df9b84ad2acd38430
Mohammad Douglas <douglasmohammed2@gmail.com>
2026-07-02 22:42:29 +0200

    Constructed game logic

--- character.js
+++ character.js
@@ -1,39 +1,29 @@
 let isJumping = false;
 let position = { x: 0, y: 0 };
-let velocity = { x: 0, y: 0 };
-const gravity = 400;
+let velocity = 0;
+const gravity = 10;
 const jumpStrength = -300;
 const groundLevel = 0;
 
 function jump() {
     if (isJumping) return;
     isJumping = true;
-    velocity.y = jumpStrength;
+    velocity = jumpStrength;
 }
 
 function updateCharacterPosition(deltaTime) {
-    position.y += velocity.y * deltaTime;
+    position.y += velocity * deltaTime;
 }
 
 function applyGravity(deltaTime) {
-    velocity.y += gravity * deltaTime;
+    velocity += gravity * deltaTime;
 }
 
 function land() {
     isJumping = false;
-    velocity.y = 0;
+    velocity = 0;
     position.y = groundLevel;
 }
-
-/*function update(deltaTime) {
-    applyGravity.call(this, deltaTime);
-    updateCharacterPosition.call(this, deltaTime);
-    // Check for landing
-    if (position.y >= groundLevel) {
-        position.y = groundLevel;
-        land.call(this);
-    }
-}*/
 
 function update(deltaTime) {
     applyGravity(deltaTime);
@@ -45,7 +35,7 @@ function update(deltaTime) {
 
 function handleKeyboard(event) {
     const key = event.key;
-    if (key === ' ' || key === 'Spacebar') {
+    if (key === ' ' || key === 'ArrowUp') {
         event.preventDefault();
         jump();
     }
